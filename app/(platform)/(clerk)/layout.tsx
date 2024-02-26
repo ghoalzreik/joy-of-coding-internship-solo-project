da12795b18ed11cfc36e123a9bcba07e096e601f
ghoalzreik <150092103+ghoalzreik@users.noreply.github.com>
2024-02-25 19:52:48 -0700

    Delete app/(platform)/(clerk)/layout.tsx

--- app/(platform)/(clerk)/layout.tsx
+++
@@ -1,7 +0,0 @@
-const ClerkLayut = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-full flex items-center justify-center">{children}</div>
-  );
-};
-
-export default ClerkLayut;
